f0401b22df3fae1f50d022543e15ea3e0c8a6143
IvanSP04 <ivan.sterlingp@uniagustiniana.edu.co>
2026-04-24 05:14:18 +0000

    otro cambio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Heroapp2.0/', // 🔥 ESTO ES LO QUE FALTABA
 })
